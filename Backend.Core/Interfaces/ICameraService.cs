4214863dbbbb35d3ccaf05b02b6744054afb0333
Eugene Cherepko <evgeniiycherepko@gmail.com>
2023-02-25 14:25:27 +0200

    camera controller and camera interface added

---
+++ Backend.Core/Interfaces/ICameraService.cs
@@ -0,0 +1,18 @@
+﻿using Backend.Core.Models;
+using Backend.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Core.Interfaces
+{
+    public interface ICameraService
+    {
+        Task<List<Camera>> GetCameras();
+        Task<Camera?> GetCameraById(int id);
+        Task<List<CameraCard>> GetCameraCards();
+        Task<CameraCard?> GetCameraCardById(int id);
+    }
+}
---
+++ Backend/Controllers/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using Backend.Core.Interfaces;
+using Backend.Core.Models;
+using Backend.Infrastructure.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Backend.Controllers
+{
+    [Route("cameras")]
+    [ApiController]
+    public class CameraController : Controller
+    {
+        private readonly ICameraService _cameraService;
+        public CameraController(ICameraService cameraService)
+        {
+            _cameraService = cameraService;
+        }
+
+        [HttpGet()]
+        public async Task<ActionResult<List<Camera>>> Get()
+        {
+            List<Camera> activities = await _cameraService.GetCameras();
+            return Ok(activities);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Camera>>> GetById(int id)
+        {
+            Camera? activities = await _cameraService.GetCameraById(id);
+            return Ok(activities);
+        }
+
+        [HttpGet("cards")]
+        public async Task<ActionResult<List<Camera>>> GetCards()
+        {
+            List<CameraCard> activities = await _cameraService.GetCameraCards();
+            return Ok(activities);
+        }
+
+        [HttpGet("cards/{id}")]
+        public async Task<ActionResult<List<Camera>>> GetCardbyId(int id)
+        {
+            CameraCard? activities = await _cameraService.GetCameraCardById(id);
+            return Ok(activities);
+        }
+    }
+}
